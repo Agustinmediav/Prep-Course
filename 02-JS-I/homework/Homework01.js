a847a6cbdf28dcdc9182319c419dfaca2e4d7b13
Agustinmediav <agustinmediavilla6@gmail.com>
2022-05-17 06:37:51 -0300

    primer commit jsi

---
+++ 02-JS-I/homework/Homework01.js
@@ -0,0 +1,8 @@
+//  Variable: Es una forma de asignarle un valor a algo
+// Funciones: Agrupan o guardan codigos para luego ser imbocado y asi evitar que se el mismo se repita, 
+//      *A la funcion le asignamos un NOMBRE', el cual lleva un 'Argumento' entre parentesis
+//         *Argumento: es un valor que le damos al nombre de la funcion
+//      *Retourn: es una declaracion que finaliza la ejecucion de de la funcion y que espesifica un valor para delvolver al imbocar la funcion
+// Declaracion: es una definicion que contiene instrucciones
+//         if: es una sentencia el cual compara operadores para verificar si es true o false
+// Valores booleanos: es un tipo de dato que define si es verdadero 'true' o falso 'false
